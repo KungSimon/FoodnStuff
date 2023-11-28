99092b9dcc8ce426f02c94160c5ac0895cd637be
StonesRocks <68532873+StonesRocks@users.noreply.github.com>
2023-11-28 14:24:50 +0100

    Update FileManager.cs
    Added JSON Export

--- FoodnStuff/FileManager.cs
+++ FoodnStuff/FileManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FoodnStuff
@@ -33,6 +35,27 @@ namespace FoodnStuff
         public void SaveBankManager()
         {
             // bankAccounts
+        }
+
+        // Method to export class objects to JSON
+        public void ExportToJson(Object _object, string baseName, string filePath = "")
+        {
+            filePath = Path.GetFullPath(filePath); // I think this does what i think it does
+            string jsonString = JsonSerializer.Serialize(_object);
+            if (filePath == "")
+            {
+                filePath = Path.Combine("jsonFolder", $"{baseName}.json");
+            }
+            CreateFolder(filePath);
+            // Write the JSON string to the file.
+            File.WriteAllText(filePath, jsonString);
+        }
+        public void CreateFolder(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
         }
     }
 }
