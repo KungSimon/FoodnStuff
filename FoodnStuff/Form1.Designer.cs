12e9972f0234dc0eae824519480da010a4ae6c17
KungSimon <simon.naslund@hotmail.com>
2023-12-14 14:16:37 +0100

    Aesthetic update...

--- FoodnStuff/Form1.Designer.cs
+++ FoodnStuff/Form1.Designer.cs
@@ -54,6 +54,7 @@
             usernameLabel = new Label();
             passwordTextBox = new TextBox();
             usernameTextBox = new TextBox();
+            label1 = new Label();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)quantityNumericUpDown).BeginInit();
@@ -67,47 +68,44 @@
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Controls.Add(tabPage3);
             tabControl1.Location = new Point(1, 0);
-            tabControl1.Margin = new Padding(3, 2, 3, 2);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(869, 394);
+            tabControl1.Size = new Size(993, 525);
             tabControl1.TabIndex = 0;
             tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
             // 
             // tabPage1
             // 
             tabPage1.BackgroundImage = (Image)resources.GetObject("tabPage1.BackgroundImage");
+            tabPage1.Controls.Add(label1);
             tabPage1.Controls.Add(quantityNumericUpDown);
             tabPage1.Controls.Add(addToCartButton);
             tabPage1.Controls.Add(itemsListBox);
             tabPage1.Controls.Add(searchLabel);
             tabPage1.Controls.Add(searchTextBox);
             tabPage1.Controls.Add(catagoryListBox);
-            tabPage1.Location = new Point(4, 24);
-            tabPage1.Margin = new Padding(3, 2, 3, 2);
+            tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3, 2, 3, 2);
-            tabPage1.Size = new Size(861, 366);
+            tabPage1.Padding = new Padding(3, 3, 3, 3);
+            tabPage1.Size = new Size(985, 492);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Items";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // quantityNumericUpDown
             // 
-            quantityNumericUpDown.Location = new Point(678, 21);
-            quantityNumericUpDown.Margin = new Padding(3, 2, 3, 2);
+            quantityNumericUpDown.Location = new Point(775, 28);
             quantityNumericUpDown.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
             quantityNumericUpDown.Name = "quantityNumericUpDown";
-            quantityNumericUpDown.Size = new Size(131, 23);
+            quantityNumericUpDown.Size = new Size(150, 27);
             quantityNumericUpDown.TabIndex = 6;
             quantityNumericUpDown.ValueChanged += quantityNumericUpDown_ValueChanged;
             // 
             // addToCartButton
             // 
-            addToCartButton.Location = new Point(123, 65);
-            addToCartButton.Margin = new Padding(3, 2, 3, 2);
+            addToCartButton.Location = new Point(141, 87);
             addToCartButton.Name = "addToCartButton";
-            addToCartButton.Size = new Size(110, 22);
+            addToCartButton.Size = new Size(126, 29);
             addToCartButton.TabIndex = 5;
             addToCartButton.Text = "Add to cart";
             addToCartButton.UseVisualStyleBackColor = true;
@@ -116,11 +114,10 @@
             // itemsListBox
             // 
             itemsListBox.FormattingEnabled = true;
-            itemsListBox.ItemHeight = 15;
-            itemsListBox.Location = new Point(365, 65);
-            itemsListBox.Margin = new Padding(3, 2, 3, 2);
+            itemsListBox.ItemHeight = 20;
+            itemsListBox.Location = new Point(417, 87);
             itemsListBox.Name = "itemsListBox";
-            itemsListBox.Size = new Size(445, 289);
+            itemsListBox.Size = new Size(508, 384);
             itemsListBox.TabIndex = 4;
             itemsListBox.Visible = false;
             itemsListBox.SelectedIndexChanged += itemsListBox_SelectedIndexChanged;
@@ -128,18 +125,17 @@
             // searchLabel
             // 
             searchLabel.AutoSize = true;
-            searchLabel.Location = new Point(10, 20);
+            searchLabel.Location = new Point(11, 27);
             searchLabel.Name = "searchLabel";
-            searchLabel.Size = new Size(42, 15);
+            searchLabel.Size = new Size(53, 20);
             searchLabel.TabIndex = 2;
             searchLabel.Text = "Search";
             // 
             // searchTextBox
             // 
-            searchTextBox.Location = new Point(80, 20);
-            searchTextBox.Margin = new Padding(3, 2, 3, 2);
+            searchTextBox.Location = new Point(91, 27);
             searchTextBox.Name = "searchTextBox";
-            searchTextBox.Size = new Size(220, 23);
+            searchTextBox.Size = new Size(251, 27);
             searchTextBox.TabIndex = 1;
             searchTextBox.MouseClick += searchTextBox_MouseClick;
             searchTextBox.TextChanged += searchTextBox_TextChanged;
@@ -148,11 +144,10 @@
             // catagoryListBox
             // 
             catagoryListBox.FormattingEnabled = true;
-            catagoryListBox.ItemHeight = 15;
-            catagoryListBox.Location = new Point(10, 110);
-            catagoryListBox.Margin = new Padding(3, 2, 3, 2);
+            catagoryListBox.ItemHeight = 20;
+            catagoryListBox.Location = new Point(11, 147);
             catagoryListBox.Name = "catagoryListBox";
-            catagoryListBox.Size = new Size(336, 244);
+            catagoryListBox.Size = new Size(383, 324);
             catagoryListBox.TabIndex = 0;
             catagoryListBox.SelectedIndexChanged += catagoryListBox_SelectedIndexChanged;
             // 
@@ -164,11 +159,10 @@
             tabPage2.Controls.Add(removeItemFromCartButton);
             tabPage2.Controls.Add(cartLabel);
             tabPage2.Controls.Add(cartListBox);
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Margin = new Padding(3, 2, 3, 2);
+            tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3, 2, 3, 2);
-            tabPage2.Size = new Size(861, 366);
+            tabPage2.Padding = new Padding(3, 3, 3, 3);
+            tabPage2.Size = new Size(985, 492);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Cart";
             tabPage2.UseVisualStyleBackColor = true;
@@ -176,19 +170,18 @@
             // totalCostLabel
             // 
             totalCostLabel.AutoSize = true;
-            totalCostLabel.Location = new Point(27, 323);
+            totalCostLabel.Location = new Point(31, 431);
             totalCostLabel.Name = "totalCostLabel";
-            totalCostLabel.Size = new Size(66, 15);
+            totalCostLabel.Size = new Size(84, 20);
             totalCostLabel.TabIndex = 4;
             totalCostLabel.Text = "Total cost:  ";
             totalCostLabel.Click += totalCostLabel_Click;
             // 
             // proceedToCheckoutButton
             // 
-            proceedToCheckoutButton.Location = new Point(301, 89);
-            proceedToCheckoutButton.Margin = new Padding(3, 2, 3, 2);
+            proceedToCheckoutButton.Location = new Point(344, 119);
             proceedToCheckoutButton.Name = "proceedToCheckoutButton";
-            proceedToCheckoutButton.Size = new Size(159, 22);
+            proceedToCheckoutButton.Size = new Size(182, 29);
             proceedToCheckoutButton.TabIndex = 3;
             proceedToCheckoutButton.Text = "Proceed to checkout";
             proceedToCheckoutButton.UseVisualStyleBackColor = true;
@@ -196,10 +189,9 @@
             // 
             // removeItemFromCartButton
             // 
-            removeItemFromCartButton.Location = new Point(278, 284);
-            removeItemFromCartButton.Margin = new Padding(3, 2, 3, 2);
+            removeItemFromCartButton.Location = new Point(318, 379);
             removeItemFromCartButton.Name = "removeItemFromCartButton";
-            removeItemFromCartButton.Size = new Size(159, 22);
+            removeItemFromCartButton.Size = new Size(182, 29);
             removeItemFromCartButton.TabIndex = 2;
             removeItemFromCartButton.Text = "Remove item from cart";
             removeItemFromCartButton.UseVisualStyleBackColor = true;
@@ -208,20 +200,19 @@
             // cartLabel
             // 
             cartLabel.AutoSize = true;
-            cartLabel.Location = new Point(27, 31);
+            cartLabel.Location = new Point(31, 41);
             cartLabel.Name = "cartLabel";
-            cartLabel.Size = new Size(54, 15);
+            cartLabel.Size = new Size(67, 20);
             cartLabel.TabIndex = 1;
             cartLabel.Text = "Your cart";
             // 
             // cartListBox
             // 
             cartListBox.FormattingEnabled = true;
-            cartListBox.ItemHeight = 15;
-            cartListBox.Location = new Point(27, 48);
-            cartListBox.Margin = new Padding(3, 2, 3, 2);
+            cartListBox.ItemHeight = 20;
+            cartListBox.Location = new Point(31, 64);
             cartListBox.Name = "cartListBox";
-            cartListBox.Size = new Size(230, 259);
+            cartListBox.Size = new Size(262, 344);
             cartListBox.TabIndex = 0;
             cartListBox.SelectedIndexChanged += cartListBox_SelectedIndexChanged;
             // 
@@ -238,20 +229,18 @@
             tabPage3.Controls.Add(usernameLabel);
             tabPage3.Controls.Add(passwordTextBox);
             tabPage3.Controls.Add(usernameTextBox);
-            tabPage3.Location = new Point(4, 24);
-            tabPage3.Margin = new Padding(3, 2, 3, 2);
+            tabPage3.Location = new Point(4, 29);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(861, 366);
+            tabPage3.Size = new Size(985, 492);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Log In";
             tabPage3.UseVisualStyleBackColor = true;
             // 
             // logOutButton
             // 
-            logOutButton.Location = new Point(771, 10);
-            logOutButton.Margin = new Padding(3, 2, 3, 2);
+            logOutButton.Location = new Point(452, 208);
             logOutButton.Name = "logOutButton";
-            logOutButton.Size = new Size(82, 22);
+            logOutButton.Size = new Size(94, 29);
             logOutButton.TabIndex = 15;
             logOutButton.Text = "Log out";
             logOutButton.UseVisualStyleBackColor = true;
@@ -260,10 +249,9 @@
             // 
             // logInButton
             // 
-            logInButton.Location = new Point(131, 113);
-            logInButton.Margin = new Padding(3, 2, 3, 2);
+            logInButton.Location = new Point(150, 151);
             logInButton.Name = "logInButton";
-            logInButton.Size = new Size(127, 22);
+            logInButton.Size = new Size(145, 29);
             logInButton.TabIndex = 8;
             logInButton.Text = "Login";
             logInButton.UseVisualStyleBackColor = true;
@@ -273,18 +261,17 @@
             // 
             workerLabel.AutoSize = true;
             workerLabel.ForeColor = SystemColors.ControlText;
-            workerLabel.Location = new Point(718, 274);
+            workerLabel.Location = new Point(821, 365);
             workerLabel.Name = "workerLabel";
-            workerLabel.Size = new Size(96, 15);
+            workerLabel.Size = new Size(122, 20);
             workerLabel.TabIndex = 7;
             workerLabel.Text = "To workers log in";
             // 
             // logInWorkerButton
             // 
-            logInWorkerButton.Location = new Point(704, 304);
-            logInWorkerButton.Margin = new Padding(3, 2, 3, 2);
+            logInWorkerButton.Location = new Point(805, 405);
             logInWorkerButton.Name = "logInWorkerButton";
-            logInWorkerButton.Size = new Size(140, 22);
+            logInWorkerButton.Size = new Size(160, 29);
             logInWorkerButton.TabIndex = 6;
             logInWorkerButton.Text = "Log In";
             logInWorkerButton.UseVisualStyleBackColor = true;
@@ -294,18 +281,17 @@
             // 
             registerLabel.AutoSize = true;
             registerLabel.ForeColor = SystemColors.ControlText;
-            registerLabel.Location = new Point(15, 274);
+            registerLabel.Location = new Point(17, 365);
             registerLabel.Name = "registerLabel";
-            registerLabel.Size = new Size(265, 15);
+            registerLabel.Size = new Size(335, 20);
             registerLabel.TabIndex = 5;
             registerLabel.Text = "Dont have a account? Register for exclusive deals";
             // 
             // registerButton
             // 
-            registerButton.Location = new Point(68, 304);
-            registerButton.Margin = new Padding(3, 2, 3, 2);
+            registerButton.Location = new Point(78, 405);
             registerButton.Name = "registerButton";
-            registerButton.Size = new Size(140, 22);
+            registerButton.Size = new Size(160, 29);
             registerButton.TabIndex = 4;
             registerButton.Text = "Register";
             registerButton.UseVisualStyleBackColor = true;
@@ -314,44 +300,50 @@
             // passwordLabel
             // 
             passwordLabel.AutoSize = true;
-            passwordLabel.Location = new Point(44, 75);
+            passwordLabel.Location = new Point(50, 100);
             passwordLabel.Name = "passwordLabel";
-            passwordLabel.Size = new Size(60, 15);
+            passwordLabel.Size = new Size(73, 20);
             passwordLabel.TabIndex = 3;
             passwordLabel.Text = "Password:";
             // 
             // usernameLabel
             // 
             usernameLabel.AutoSize = true;
-            usernameLabel.Location = new Point(44, 27);
+            usernameLabel.Location = new Point(50, 36);
             usernameLabel.Name = "usernameLabel";
-            usernameLabel.Size = new Size(63, 15);
+            usernameLabel.Size = new Size(78, 20);
             usernameLabel.TabIndex = 2;
             usernameLabel.Text = "Username:";
             // 
             // passwordTextBox
             // 
-            passwordTextBox.Location = new Point(122, 70);
-            passwordTextBox.Margin = new Padding(3, 2, 3, 2);
+            passwordTextBox.Location = new Point(139, 93);
             passwordTextBox.Name = "passwordTextBox";
-            passwordTextBox.Size = new Size(162, 23);
+            passwordTextBox.Size = new Size(185, 27);
             passwordTextBox.TabIndex = 1;
             // 
             // usernameTextBox
             // 
-            usernameTextBox.Location = new Point(122, 26);
-            usernameTextBox.Margin = new Padding(3, 2, 3, 2);
+            usernameTextBox.Location = new Point(139, 35);
             usernameTextBox.Name = "usernameTextBox";
-            usernameTextBox.Size = new Size(162, 23);
+            usernameTextBox.Size = new Size(185, 27);
             usernameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(701, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Quantity:";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(868, 396);
+            ClientSize = new Size(992, 528);
             Controls.Add(tabControl1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             FormClosing += Form1_FormClosing_1;
@@ -394,5 +386,6 @@
         private Button addToCartButton;
         private Label totalCostLabel;
         private NumericUpDown quantityNumericUpDown;
+        private Label label1;
     }
 }
--- FoodnStuff/Shipping.Designer.cs
+++ FoodnStuff/Shipping.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Shipping));
             addButton = new Button();
             transportsListBox = new ListBox();
             ordersListBox = new ListBox();
@@ -42,10 +43,9 @@
             // 
             // addButton
             // 
-            addButton.Location = new Point(133, 265);
-            addButton.Margin = new Padding(3, 2, 3, 2);
+            addButton.Location = new Point(152, 353);
             addButton.Name = "addButton";
-            addButton.Size = new Size(85, 22);
+            addButton.Size = new Size(97, 29);
             addButton.TabIndex = 5;
             addButton.Text = "Add";
             addButton.UseVisualStyleBackColor = true;
@@ -54,41 +54,39 @@
             // transportsListBox
             // 
             transportsListBox.FormattingEnabled = true;
-            transportsListBox.ItemHeight = 15;
-            transportsListBox.Location = new Point(224, 19);
-            transportsListBox.Margin = new Padding(3, 2, 3, 2);
+            transportsListBox.ItemHeight = 20;
+            transportsListBox.Location = new Point(256, 25);
             transportsListBox.Name = "transportsListBox";
-            transportsListBox.Size = new Size(304, 244);
+            transportsListBox.Size = new Size(347, 324);
             transportsListBox.TabIndex = 4;
             transportsListBox.SelectedIndexChanged += transportsListBox_SelectedIndexChanged;
             // 
             // ordersListBox
             // 
             ordersListBox.FormattingEnabled = true;
-            ordersListBox.ItemHeight = 15;
-            ordersListBox.Location = new Point(18, 19);
-            ordersListBox.Margin = new Padding(3, 2, 3, 2);
+            ordersListBox.ItemHeight = 20;
+            ordersListBox.Location = new Point(21, 25);
             ordersListBox.Name = "ordersListBox";
-            ordersListBox.Size = new Size(200, 244);
+            ordersListBox.Size = new Size(228, 324);
             ordersListBox.TabIndex = 3;
             ordersListBox.SelectedIndexChanged += ordersListBox_SelectedIndexChanged;
             // 
             // readyToBeShippedListBox
             // 
             readyToBeShippedListBox.FormattingEnabled = true;
-            readyToBeShippedListBox.ItemHeight = 15;
-            readyToBeShippedListBox.Location = new Point(533, 20);
-            readyToBeShippedListBox.Margin = new Padding(2);
+            readyToBeShippedListBox.ItemHeight = 20;
+            readyToBeShippedListBox.Location = new Point(609, 27);
+            readyToBeShippedListBox.Margin = new Padding(2, 3, 2, 3);
             readyToBeShippedListBox.Name = "readyToBeShippedListBox";
-            readyToBeShippedListBox.Size = new Size(200, 244);
+            readyToBeShippedListBox.Size = new Size(228, 324);
             readyToBeShippedListBox.TabIndex = 8;
             // 
             // shipOrderButton
             // 
-            shipOrderButton.Location = new Point(648, 265);
-            shipOrderButton.Margin = new Padding(2);
+            shipOrderButton.Location = new Point(741, 353);
+            shipOrderButton.Margin = new Padding(2, 3, 2, 3);
             shipOrderButton.Name = "shipOrderButton";
-            shipOrderButton.Size = new Size(85, 22);
+            shipOrderButton.Size = new Size(97, 29);
             shipOrderButton.TabIndex = 10;
             shipOrderButton.Text = "Ship order";
             shipOrderButton.UseVisualStyleBackColor = true;
@@ -97,10 +95,10 @@
             // 
             // removeFromReadyToBeShippedButton
             // 
-            removeFromReadyToBeShippedButton.Location = new Point(533, 265);
-            removeFromReadyToBeShippedButton.Margin = new Padding(2);
+            removeFromReadyToBeShippedButton.Location = new Point(609, 353);
+            removeFromReadyToBeShippedButton.Margin = new Padding(2, 3, 2, 3);
             removeFromReadyToBeShippedButton.Name = "removeFromReadyToBeShippedButton";
-            removeFromReadyToBeShippedButton.Size = new Size(85, 22);
+            removeFromReadyToBeShippedButton.Size = new Size(97, 29);
             removeFromReadyToBeShippedButton.TabIndex = 11;
             removeFromReadyToBeShippedButton.Text = "Remove";
             removeFromReadyToBeShippedButton.UseVisualStyleBackColor = true;
@@ -109,10 +107,10 @@
             // 
             // deleteOrderButton
             // 
-            deleteOrderButton.Location = new Point(18, 265);
+            deleteOrderButton.Location = new Point(21, 353);
             deleteOrderButton.Margin = new Padding(0);
             deleteOrderButton.Name = "deleteOrderButton";
-            deleteOrderButton.Size = new Size(85, 22);
+            deleteOrderButton.Size = new Size(97, 29);
             deleteOrderButton.TabIndex = 12;
             deleteOrderButton.Text = "Delete order";
             deleteOrderButton.UseVisualStyleBackColor = true;
@@ -121,20 +119,20 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(83, 4);
+            label1.Location = new Point(95, 5);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(42, 15);
+            label1.Size = new Size(53, 20);
             label1.TabIndex = 13;
             label1.Text = "Orders";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(340, 4);
+            label2.Location = new Point(389, 5);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(61, 15);
+            label2.Size = new Size(77, 20);
             label2.TabIndex = 14;
             label2.Text = "Transports";
             label2.Visible = false;
@@ -142,19 +140,20 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(533, 4);
+            label3.Location = new Point(609, 5);
             label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(154, 15);
+            label3.Size = new Size(195, 20);
             label3.TabIndex = 15;
             label3.Text = "Orders loaded on transports";
             label3.Visible = false;
             // 
             // Shipping
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(774, 314);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(885, 419);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -165,7 +164,6 @@
             Controls.Add(addButton);
             Controls.Add(transportsListBox);
             Controls.Add(ordersListBox);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Shipping";
             Text = "Shipping";
             ResumeLayout(false);
